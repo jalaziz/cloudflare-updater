b6ae52a723308a43ecd57a7a2600e15d8f1b8b94
Jameel Al-Aziz <me@jalaziz.net>
2014-04-13 19:08:16 -0700

    Add ProxyFix middleware

--- update.py
+++ update.py
@@ -2,9 +2,11 @@ import re
 
 from flask import Flask, request
 from pyflare import Pyflare
+from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 @app.errorhandler(Pyflare.APIError)
